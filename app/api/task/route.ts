6dfb130d283def6d655cd5dad173e9a9577bd711
pmmasud111 <pmmasud111@gmail.com>
2024-11-08 15:11:27 +0600

    fixe build error

--- app/api/task/route.ts
+++ app/api/task/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-export async function GET(request: Request) {
+export async function GET() {
   try {
     const tasks = await prisma.task.findMany({
       include: {
