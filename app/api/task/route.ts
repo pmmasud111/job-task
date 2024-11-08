661aaf33b7833f6cc40b99deabac56469204d2dc
pmmasud111 <pmmasud111@gmail.com>
2024-11-08 15:06:05 +0600

    task create and upload file api added

---
+++ app/api/task/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export async function GET(request: Request) {
+  try {
+    const tasks = await prisma.task.findMany({
+      include: {
+        files: true,
+      },
+    });
+    return NextResponse.json({ tasks });
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    return NextResponse.json(
+      { error: "Error fetching tasks" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+export async function POST(request: Request) {
+  const data = await request.json();
+  try {
+    const task = await prisma.task.create({
+      data: {
+        ...data,
+      },
+    });
+    return NextResponse.json({ task });
+  } catch (error) {
+    console.error("Error creating task:", error);
+    return NextResponse.json({ error: "Error creating task" }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
---
+++ app/api/task/uploadFile/route.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export async function POST(request: Request) {
+  const data = await request.formData();
+  const taskId = parseInt(data.get("taskId") as string);
+  if (!taskId) {
+    return new Response("Invalid task ID", { status: 400 });
+  }
+  const files = Array.from(data.getAll("files")).filter(
+    (f) => f instanceof File
+  ) as Array<File>;
+  try {
+    if (files.length > 0) {
+      for (const file of files) {
+        await prisma.files.create({
+          data: {
+            fileName: file.name,
+            extension: file.name,
+            fileSize: file.size,
+            taskId: taskId,
+          },
+        });
+      }
+      return new Response("Files uploaded successfully", { status: 200 });
+    } else {
+      return new Response("No files uploaded", { status: 400 });
+    }
+  } catch (error) {
+    console.error("Error uploading files:", error);
+    return new Response("Error uploading files", { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
